1c668179baa321202d949ffc8a0924b7df120604
김기영 <cla12093@gmail.com>
2025-07-17 19:29:56 +0900

    [Gold II] Title: 합이 0인 네 정수, Time: 4268 ms, Memory: 275504 KB -BaekjoonHub

---
+++ 백준/Gold/7453. 합이 0인 네 정수/합이 0인 네 정수.kt
@@ -0,0 +1,62 @@
+import java.util.*
+
+fun main() {
+    val br = System.`in`.bufferedReader()
+    val n = br.readLine().toInt()
+    val arr = Array<LongArray>(4){ LongArray(n) }
+    var count = 0L
+    val arrEndpointIndex = n*n-1
+
+    repeat(n){ nIter ->
+        val st = StringTokenizer(br.readLine())
+        repeat(4){ fourIter ->
+            arr[fourIter][nIter] = st.nextToken().toLong()
+        }
+    }
+
+    val arr1 = LongArray(arrEndpointIndex+1)
+    val arr2 = LongArray(arrEndpointIndex+1)
+
+    repeat(n){ i ->
+        repeat(n){ j ->
+            arr1[n*i+j] = arr[0][i] + arr[1][j]
+            arr2[n*i+j] = arr[2][i] + arr[3][j]
+        }
+    }
+
+    arr1.sort()
+    arr2.sort()
+
+    var arr1Pointer = 0
+    var arr2Pointer = arrEndpointIndex
+
+    while (arr1Pointer <= arrEndpointIndex && arr2Pointer >= 0){
+        val sum = arr1[arr1Pointer] + arr2[arr2Pointer]
+        if(sum == 0L){
+            var arr1SameNumCount = 0L;
+            var arr2SameNumCount = 0L;
+            var arr1PointerTemp = arr1Pointer
+            var arr2PointerTemp = arr2Pointer
+
+            while(arr1PointerTemp <= arrEndpointIndex && arr1[arr1Pointer] == arr1[arr1PointerTemp]) {
+                arr1SameNumCount++
+                arr1PointerTemp++
+            }
+            while(arr2PointerTemp >= 0 && arr2[arr2Pointer] == arr2[arr2PointerTemp]){
+                arr2SameNumCount++
+                arr2PointerTemp--
+            }
+
+            count += arr1SameNumCount * arr2SameNumCount
+
+            arr1Pointer = arr1PointerTemp
+            arr2Pointer = arr2PointerTemp
+        } else if(sum < 0){
+            arr1Pointer++
+        } else {
+            arr2Pointer--
+        }
+    }
+
+    println(count)
+}
